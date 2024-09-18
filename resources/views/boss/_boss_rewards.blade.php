715818e056ca7f432379ead72abd43036bfc3fd6
ScuffedNewt <dragonminer101@gmail.com>
2024-09-18 09:21:43 +0100

    fix(rewards): account for user type reward claims / thresholds

--- resources/views/boss/_boss_rewards.blade.php
+++ resources/views/boss/_boss_rewards.blade.php
@@ -32,7 +32,11 @@
             @endforeach
         @else
             @php
-                $damagePercentage = (($boss->total_health - $boss->current_health) / $boss->total_health) * 100;
+                if ($boss->type == 'Global'|| !Auth::check()) {
+                        $damagePercentage = round((($boss->total_health - $boss->current_health) / $boss->total_health) * 100);
+                } else {
+                    $damagePercentage = round((($boss->total_health - $boss->getLogs(Auth::user())->sum('damage')) / $boss->total_health) * 100);
+                }
 
                 $rewards = $boss
                     ->rewards()
