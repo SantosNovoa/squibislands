e82a55085fa51efa5728e754747c0a5334a19a7d
ScuffedNewt <ScuffedNewt@users.noreply.github.com>
2024-09-18 08:22:42 +0000

    refactor: fix blade formatting

--- resources/views/boss/_boss_rewards.blade.php
+++ resources/views/boss/_boss_rewards.blade.php
@@ -32,8 +32,8 @@
             @endforeach
         @else
             @php
-                if ($boss->type == 'Global'|| !Auth::check()) {
-                        $damagePercentage = round((($boss->total_health - $boss->current_health) / $boss->total_health) * 100);
+                if ($boss->type == 'Global' || !Auth::check()) {
+                    $damagePercentage = round((($boss->total_health - $boss->current_health) / $boss->total_health) * 100);
                 } else {
                     $damagePercentage = round((($boss->total_health - $boss->getLogs(Auth::user())->sum('damage')) / $boss->total_health) * 100);
                 }
