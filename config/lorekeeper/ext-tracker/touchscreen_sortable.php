1b3a724168a7cfa58288035e2b8483b6adff7c19
liwoyadan <liwoyadan@users.noreply.github.com>
2024-11-14 06:04:55 +0000

    refactor: fix PHP styling

--- config/lorekeeper/ext-tracker/touchscreen_sortable.php
+++ config/lorekeeper/ext-tracker/touchscreen_sortable.php
@@ -3,7 +3,7 @@
 return [
     'wiki_key' => 'Touchscreen_Sortable',
     'creators' => json_encode([
-        'Min' => 'https://github.com/liwoyadan/',
+        'Min'         => 'https://github.com/liwoyadan/',
         'Touch Punch' => 'https://github.com/furf/jquery-ui-touch-punch',
     ]),
     'version'  => '3.0.0',
