526d7dd54d41bc964225e5ff6fce966d28c7fbc1
min <liwoyadans@gmail.com>
2024-11-13 22:01:33 -0800

    chore: editing credits

---
+++ config/lorekeeper/ext-tracker/touchscreen_sortable.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'wiki_key' => 'Touchscreen_Sortable',
+    'creators' => json_encode([
+        'Min' => 'https://github.com/liwoyadan/',
+        'Touch Punch' => 'https://github.com/furf/jquery-ui-touch-punch',
+    ]),
+    'version'  => '3.0.0',
+];
