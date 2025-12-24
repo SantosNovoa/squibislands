a4368c77bbd7efa22bcd40a2710bb2a324f32813
SantosNovoa <novoasj8729@student.laccd.edu>
2025-12-24 04:12:04 -0500

    feature new command for regen thumbnails

---
+++ app/Console/Commands/RegenerateGalleryThumbnails.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Gallery\GallerySubmission;
+use Illuminate\Console\Command;
+use Intervention\Image\Facades\Image;
+
+class RegenerateGalleryThumbnails extends Command
+{
+    protected $signature = 'thumbnails:regenerate-gallery';
+    protected $description = 'Regenerates all gallery submission thumbnails';
+
+    public function handle()
+    {
+        $submissions = GallerySubmission::whereNotNull('hash')->get();
+        $this->info("Regenerating {$submissions->count()} gallery thumbnails...");
+        
+        $bar = $this->output->createProgressBar($submissions->count());
+        $bar->start();
+
+        foreach ($submissions as $submission) {
+            try {
+                if (file_exists($submission->imagePath.'/'.$submission->imageFileName)) {
+                    Image::make($submission->imagePath.'/'.$submission->imageFileName)
+                        ->fit(
+                            config('lorekeeper.settings.masterlist_thumbnails.width'),
+                            config('lorekeeper.settings.masterlist_thumbnails.height')
+                        )
+                        ->save($submission->thumbnailPath.'/'.$submission->thumbnailFileName);
+                }
+            } catch (\Exception $e) {
+                $this->error("\nFailed for submission #{$submission->id}: " . $e->getMessage());
+            }
+            
+            $bar->advance();
+        }
+
+        $bar->finish();
+        $this->newLine();
+        $this->info('Gallery thumbnails regenerated successfully!');
+    }
+}
