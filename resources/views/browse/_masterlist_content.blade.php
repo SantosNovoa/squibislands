3ab3b09cca5cbb1f432ca9a60c873d47c0f16b80
SantosNovoa <novoasj8729@student.laccd.edu>
2025-12-27 02:42:10 -0500

    refactor bootstrap classes on masterlist

--- resources/views/browse/_masterlist_content.blade.php
+++ resources/views/browse/_masterlist_content.blade.php
@@ -140,13 +140,13 @@
 {!! $characters->render() !!}
 <div id="gridView" class="hide">
     @foreach ($characters->chunk(4) as $chunk)
-        <div class="row">
+        <div class="row p-4">
             @foreach ($chunk as $character)
                 <div class="col-md-3 col-6 text-center">
                     <div class="masterlist-thumbnail">
                         <a href="{{ $character->url }}"><img src="{{ $character->image->thumbnailUrl }}" class="img-thumbnail" alt="Thumbnail for {{ $character->fullName }}" /></a>
                     </div>
-                    <div class="mt-1">
+                    <div class="mt-1 pt-3">
                         <a href="{{ $character->url }}" class="h5 mb-0">
                             @if (!$character->is_visible)
                                 <i class="fas fa-eye-slash"></i>
