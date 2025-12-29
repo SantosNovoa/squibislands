48fd2fa987f2619fcdaf644c00f7930208aa329c
SantosNovoa <novoasj8729@student.laccd.edu>
2025-12-29 10:47:17 -0500

    style added a fixed width to shop modal images

--- resources/views/shops/_stock_modal.blade.php
+++ resources/views/shops/_stock_modal.blade.php
@@ -5,7 +5,7 @@
     <div class="text-center mb-3">
         <div class="mb-1">
             @if ($stock->item->has_image)
-                <a href="{{ $stock->item->idUrl }}"><img src="{{ $stock->item->imageUrl }}" alt="{{ $stock->item->name }}" /></a>
+                <a href="{{ $stock->item->idUrl }}"><img src="{{ $stock->item->imageUrl }}" alt="{{ $stock->item->name }}" style="max-width: 400px;" /></a>
             @endif
         </div>
         <div><a href="{{ $stock->item->idUrl }}"><strong>{{ $stock->item->name }}</strong></a></div>
